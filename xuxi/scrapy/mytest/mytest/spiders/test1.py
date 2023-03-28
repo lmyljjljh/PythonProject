d14ca57134ba1b9826e1ae1daf316dd4e6683e90
lmyljjljh <1946483721@qq.com>
2023-03-28 16:44:59 +0800

    2023.03.28

--- xuxi/scrapy/mytest/mytest/spiders/test1.py
+++ xuxi/scrapy/mytest/mytest/spiders/test1.py
@@ -3,8 +3,8 @@ import scrapy
 
 class Test1Spider(scrapy.Spider):
     name = "test1"
-    allowed_domains = ["itcase.cn"]
-    start_urls = ["http://itcase.cn/"]
+    allowed_domains = ["itcast.cn"]
+    start_urls = ["http://itcast.cn/"]
 
     def parse(self, response):
         pass
