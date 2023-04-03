8a4ad308ac8f65c62544d44c21d46fce0ab08168
lmyljjljh <1946483721@qq.com>
2023-04-03 09:41:05 +0800

    2023.04.03

--- 蓝桥杯/翻硬币.py
+++ 蓝桥杯/翻硬币.py
@@ -1,2 +1,15 @@
 s1 = input()
 s2 = input()
+
+start_n = 0
+f = 0
+summ = 0
+for i in range(len(s1)):
+    if s1[i] != s2[i]:
+        if f == 0:
+            start_n = i
+            f = 1
+        else:
+            summ += i - start_n
+            f = 0
+print(summ)
