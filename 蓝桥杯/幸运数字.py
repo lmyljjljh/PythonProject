e8419921808bdfcd37489f4ab38cfbb88e6c1340
lmyljjljh <1946483721@qq.com>
2023-04-03 13:22:06 +0800

    2023.04.03

--- 蓝桥杯/幸运数字.py
+++ 蓝桥杯/幸运数字.py
@@ -1,9 +1,9 @@
 m, n = map(int, input().split())
 
 lista = range(1, 1000000, 2)
-listc = [x for x in range(1, len(lista) + 1) if x % 3 != 0]
+listc = [lista[x-1] for x in range(1, len(lista) + 1) if x % 3 != 0]
 
-liste = [x for x in range(1, len(listc) + 1) if x % 7 != 0]
+liste = [listc[x-1] for x in range(1, len(listc) + 1) if x % 7 != 0]
 
 list1 = [i for i in liste if m < i < n]
 
